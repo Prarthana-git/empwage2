273db73681eaa7ca24a9b31d40bf77ba12590b3a
Prarthana-git <prarthanachaudhari@gmail.com>
2021-06-02 13:34:04 +0530

    Calculate DailyWage

---
+++ EmpWage.java
@@ -0,0 +1,34 @@
+package com.Bridgelabz;
+
+public class EmpWage {
+	private static String[] args;
+	public String company;
+	public int empRatePerHour;
+	public int numOfWorkingDays;
+	public int maxHoursPerMonth;
+	public int totalEmpWage;
+	
+	public EmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+		
+		public void setTotalEmpWage(int totalEmpWage) {
+			this.totalEmpWage = totalEmpWage;
+		}
+
+		@Override
+		public String toString() {
+			return "Total Emp Wage for company: "+company+" is: "+totalEmpWage;
+		}
+
+		public static void main(String[] args) {
+
+			IComputeEmpWage empWageBuilder = new EmpWageBuilderArray();
+			empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+			empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+			empWageBuilder.computeEmpWage();
+		}
+}
